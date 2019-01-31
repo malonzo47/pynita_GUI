5e9b61b9949ecab97ef15c474313ad4f571c9826
pytoPN <30472662+pytoPN@users.noreply.github.com>
2019-01-31 12:20:56 -0500

    Delete setup.py

--- setup.py
+++
@@ -1,7 +0,0 @@
-from distutils.core import setup
-from Cython.Build import cythonize
-import numpy
-
-setup(
-    ext_modules = cythonize("pynita_source/nita_funs/distance_funs/distance_funs_cython.pyx", include_path=[numpy.get_include()])
-)
