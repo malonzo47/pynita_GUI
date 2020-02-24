a0f9540b487865228435af292efa486857a2ca89
Vladimir <vladimir.dinic.work@gmail.com>
2020-02-24 05:57:27 +0100

    mac sh added

--- build_mac.sh
+++ build_mac.sh
@@ -1,1 +1,3 @@
-pyinstaller --onefile --windowed --icon icon.ico --add-data '/Library/Frameworks/Python.framework/Versions/3.7/lib/tcl8.6/':'tcl' --add-data '/Library/Frameworks/Python.framework/Versions/3.7/lib/tk8.6/':'tk' main.py
+pyinstaller --onefile --windowed --hidden-import pkg_resources.py2_warn pynita_gui_main.py
+pause
+#pyinstaller --onefile --windowed --icon icon.ico --add-data '/Library/Frameworks/Python.framework/Versions/3.7/lib/tcl8.6/':'tcl' --add-data '/Library/Frameworks/Python.framework/Versions/3.7/lib/tk8.6/':'tk' main.py
