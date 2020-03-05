523fbb9cd90429faff16a27850a6296b4579a13c
Vladimir <vladimir.dinic.work@gmail.com>
2020-03-05 17:46:24 +0100

    docker for windows modified and xlaunch config added

--- installation/docker/windows/docker_run_windows.bat
+++ installation/docker/windows/docker_run_windows.bat
@@ -1,2 +1,2 @@
 docker build . -t pynita
-docker run -it -e DISPLAY=host.docker.internal:0.0 -v %cd%:/app pynita python3 pynita_gui_main.py
+docker run -it -e DISPLAY=docker.for.win.host.internal:0 -v %cd%:/app pynita python3 pynita_gui_main.py
