90e0706c687524896adcce7b0683995fd1e81c39
PCK <pck2016217@gmail.com>
2020-03-12 08:47:21 +0100

    removed old documents

--- windows_install.bat
+++
@@ -1,5 +0,0 @@
-set env_name=pynita_env
-
-conda create -n %env_name% python=3.6 --yes && conda activate %env_name% && for /f  "tokens=*" %%i in (requirements.txt) do conda install --yes %%i
-
-pause
