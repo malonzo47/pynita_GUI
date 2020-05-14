e1a7200cf51720776bd460683a9bb1ff5774bfe6
PCK <pck2016217@gmail.com>
2020-05-14 15:41:30 +0200

    added chocolatey and c++ compiler tool for dist function compile

--- installation/source/windows/install_win32.bat
+++ installation/source/windows/install_win32.bat
@@ -1,3 +1,5 @@
+@"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+choco install visualstudio2019-workload-vctools
 pip install -r requirements_win32.txt
 cd pynita_source/nita_funs/distance_funs
 python setup.py build_ext --inplace
